c708d0208a6fc7a658b1430e7574f7cc2c9a5229
David <daviddss1999@gmail.com>
2022-06-27 13:01:34 -0400

    Completed Coding Assigment
    Committed code for coding assignment

--- ITSMCodingTest/Controllers/HomeController.cs
+++ ITSMCodingTest/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using ITSMCodingTest.Helpers;
 using ITSMCodingTest.Models;
+using Newtonsoft.Json;
 
 namespace ITSMCodingTest.Controllers
 {
@@ -25,7 +29,17 @@ namespace ITSMCodingTest.Controllers
             try
             {
                 // Read the countries.json file within the Helpers folder and map to a list of CountryView, sorted alphabetically
-                // << YOUR CODE HERE >>
+                List<CountryView> countryList= new List<CountryView>();
+
+                using (StreamReader reader = new StreamReader(Server.MapPath(@"~\Helpers\countries.json")))
+                {
+                    string countriesJson = reader.ReadToEnd();
+                    countryList = JsonConvert.DeserializeObject<List<CountryView>>(countriesJson);
+
+                    //Sort by Alphabetical Order
+                    countryList.Sort((a, b) => string.Compare(a.Name, b.Name));
+                }
+                return SuccessResult(countryList); ;
             }
             catch (Exception e)
             {
@@ -43,7 +57,14 @@ namespace ITSMCodingTest.Controllers
             try
             {
                 // Add a new AddressRecord entry to the Entities
-                // << YOUR CODE HERE >>
+                AddressRecord newRecord;
+                using (var db = new AddressBookEntities())
+                {
+                    newRecord = new AddressRecord { FirstName = "New", LastName = "New" };
+                    db.AddressRecords.Add(newRecord);
+                    db.SaveChanges();
+                }
+                return SuccessResult(newRecord.Id);
             }
             catch (Exception e)
             {
@@ -61,7 +82,12 @@ namespace ITSMCodingTest.Controllers
             try
             {
                 // Retrieve all of the records
-                // << YOUR CODE HERE >>
+                using (var db = new AddressBookEntities())
+                {
+                    //Sort by Last Name and then by First Name
+                    var records = db.AddressRecords.ToList().OrderBy(person => person.LastName).ThenBy(person => person.FirstName);
+                    return SuccessResult(records);
+                }
             }
             catch (Exception e)
             {
@@ -84,32 +110,60 @@ namespace ITSMCodingTest.Controllers
                 using (var db = new AddressBookEntities())
                 {
                     // Try to retrieve the record, and fail if the record doesn't exist
-                    // << YOUR CODE HERE >>
+                    var record = db.AddressRecords.Where(entry => entry.Id == recordData.Id).Single();
 
                     // Validate that the required fields have values
-                    // << YOUR CODE HERE >>
+                    if(String.IsNullOrEmpty(recordData.FirstName) || String.IsNullOrEmpty(recordData.LastName))
+                    {
+                        return FailedResult("First and Last Name Required");
+                    }
 
                     var allowedExtensions = new[] {".jpg", ".jpeg", ".png", ".gif", ".bmp"};
                     for (var i = 0; i < Request.Files.Count; i++)
                     {
                         // Get the file if it exists, and if not, just carry on
-                        // << YOUR CODE HERE >>
+                        var photo = Request.Files[0];
 
                         // Check that the file has a valid name
-                        // << YOUR CODE HERE >>
+                        if (String.IsNullOrEmpty(photo.FileName) ||
+                        (photo.FileName.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0)) { 
+                            return FailedResult("Invalid Image Name");
+                        }
 
                         // Validate the file size is less than 1MB
-                        // << YOUR CODE HERE >>
+                        if (photo.ContentLength >= 1000000) return FailedResult("Invalid Image Size. Max Size Allowed is 1MB");
 
                         // Check that the photo is the correct type from the allowedExtensions
-                        // << YOUR CODE HERE >>
+                        bool validExtension = false;
+                        if (photo.ContentType.Contains("image"))
+                        {
+                            validExtension = allowedExtensions.Any(item => photo.FileName.EndsWith(item, StringComparison.OrdinalIgnoreCase));
+                        }
+                        if (!validExtension) return FailedResult("Invalid Image Type or Extension");
 
                         // All is well, save the image to memory, and then drop it in the Uploads folder with the Id as the name in PNG format
-                        // << YOUR CODE HERE >>
+                        var imageExtension = Path.GetExtension(photo.FileName);
+
+                        string savedFileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Uploads");
+                        savedFileName = Path.Combine(savedFileName, Path.GetFileName(recordData.Id.ToString()+".png"));
+
+                        photo.SaveAs(savedFileName);
                     }
 
                     // Update all record properties and save changes
-                    // << YOUR CODE HERE >>
+                    record.FirstName = recordData.FirstName;
+                    record.LastName = recordData.LastName;
+                    record.Address = recordData.Address;
+                    record.AddressLine2 = recordData.AddressLine2;
+                    record.City = recordData.City;
+                    record.ProvinceState = recordData.ProvinceState;
+                    record.PostalZip = recordData.PostalZip;
+                    record.Country = recordData.Country;
+                    record.PhoneNumber = recordData.PhoneNumber;
+                    record.EmailAddress = recordData.EmailAddress;
+                    db.SaveChanges();
+
+                    return SuccessResult(recordData.Id);
                 }
             }
             catch (Exception e)
@@ -129,7 +183,13 @@ namespace ITSMCodingTest.Controllers
             try
             {
                 // Get the record to delete, and fail if the record does not exist
-                // << YOUR CODE HERE >>
+                using (var db = new AddressBookEntities())
+                {
+                    var record = db.AddressRecords.Where(entry => entry.Id == recordId).Single();
+                    db.AddressRecords.Remove(record);
+                    db.SaveChanges();
+                }
+                return SuccessResult(recordId);
             }
             catch (Exception e)
             {
--- ITSMCodingTest/Scripts/app.js
+++ ITSMCodingTest/Scripts/app.js
@@ -5,7 +5,6 @@ var currentRecord = null;
 
 
 // Generic Functions
-
 $(function() {
     $('[data-toggle="tooltip"]').tooltip();
 });
@@ -59,82 +58,283 @@ function initAddressBook() {
     $.ajax({
         url: "Home/GetAllEntries",
         type: "GET",
-        dataType: "json"
+        dataType: "json",
+        beforeSend: showLoading()
     }).done(function (data) {
         if (data.result) {
             addressBookRecords = data.resultSet;
             sortRecords();
             displayRecords();
+            hideLoading();
             return;
         }
+        hideLoading();
         toastr.error(data.error, "Failed to get Load Records");
-    }).fail(function(xhr) {
+    }).fail(function (xhr) {
+        hideLoading();
         toastr.error(xhrErrorMessage(xhr), "Failed to Load Records");
     });
 
     // Initialize the Country Selector by retriving via the GetCountries action and populating the dropdown and variable
-    // << YOUR CODE HERE >>
+    $.ajax({
+        url: "Home/GetCountries",
+        type: "GET",
+        dataType: "json",
+    }).done(function (data) {
+        if (data.result) {
+            countryList = data.resultSet;
+            countryList.forEach((country) => {
+                $("#selectCountry").append("<option value='" + country.name + "'><div>" + country.name + "</div></option>'");
+            });
+            return;
+        }
+        toastr.error(data.error, "Failed to Load Countries");
+    }).fail(function (xhr) {
+        toastr.error(xhrErrorMessage(xhr), "Failed to Load Countries");
+    });
 }
 
 // Displays the address book records, with an optional parameter to pre-select an ID of a record
 function displayRecords(preselectId) {
-    // << YOUR CODE HERE >>
+    //Clear address book records from display
+    $(".address-book-entries").empty();
+
+    //Append the address book records in display
+    addressBookRecords.forEach((element) => {
+        $(".address-book-entries").append("<a class='address-book-record font-weight-bold' onclick='selectRecord(this);' data-id='"
+            + element.id + "' href='#'><img class='record-photo mr-2' src='Uploads/"
+            + element.id + ".png' onerror='this.src = \"Content/Images/blankuser.png\"' style='border-radius: 30px;'/>"
+            + element.lastName + ", " + element.firstName + "</a>")
+    });
+
+    //Pre-select record is preselectId is given
+    if (preselectId != undefined) {
+        var preselectRecord = $(".address-book-record[data-id='" + preselectId + "']");
+        if ($(preselectRecord).length > 0) selectRecord(preselectRecord);
+    }
 }
 
 // Adds a new entry with the First and Last name of "New"
 function addNewEntry() {
-    // << YOUR CODE HERE >>
+    $.ajax({
+        url: "Home/AddEntry",
+        type: "POST",
+        beforeSend: showLoading()
+    }).done(function (data) {
+        if (data.result) {
+            addressBookRecords.push({ id: data.recordId, firstName: "New", lastName: "New" });
+            sortRecords();
+            displayRecords();
+            editRecord(data.recordId);
+            $('#readOnlyDisplay').hide();
+            $('#editableDisplay').show();            
+            hideLoading();
+            return;
+        }
+        hideLoading();
+        toastr.error(data.error, "Failed to Add Entry");
+    }).fail(function (xhr) {
+        hideLoading();
+        toastr.error(xhrErrorMessage(xhr), "Failed to Add Entry");
+    });
 }
 
 // Selects a record based on the element in the list or a record ID number
 function selectRecord(record) {
-    // Check if the record is a number or if it's our <a> element that contains the record data, and get the ID accordingly
-    // << YOUR CODE HERE >>
+    var recordID = null;
 
+    $(".address-book-record").removeClass("active");
+    $("#editButton").prop('disabled', true);
+
+    // Check if the record is a number or if it's our <a> element that contains the record data, and get the ID accordingly
+    if ($(record).is("a")) {
+        recordID = $(record).data("id");
+    }
+    else if (!isNaN(record) && record > 0) {
+        recordID = record;
+    }
     // Hide all fields, as we're only going to show them if they have data
-    // << YOUR CODE HERE >>
+    $('#displayAddressColumn, #displayContactColumn, #displayPhoneItem, #displayEmailItem, #displayAddress').hide()
+    $('#displayAddressColumn ul li').not('li:first').remove()
 
     // Once we've gotten the record from the addressBookRecords, display the content on the front end and enable the Edit button
-    // << YOUR CODE HERE >>
+    addressBookRecords.forEach((element) => {
+        if (element.id == recordID) {
+            currentRecord = element;
+            $(".address-book-record[data-id='" + currentRecord.id + "']").addClass("active");
+            $("#editButton").prop('disabled', false);
+        }
+    });
+    
+    var userPhoto = $(".address-book-record[data-id='" + currentRecord.id + "'] img").prop('src');
+    $('#displayPhoto').attr("src", userPhoto);
 
-    // Show all columns/items that have data
-    // << YOUR CODE HERE >>
+    $('#displayName').text(currentRecord.firstName + " " + currentRecord.lastName);
+
+    // Show all columns/items that have data    
+    if (currentRecord.address != null && currentRecord.address != "") { $("#displayAddressColumn, #displayAddress").show(); $('#displayAddress').text(currentRecord.address); }
+    if (currentRecord.addressLine2 != null && currentRecord.addressLine2 != "") { $("#displayAddressColumn").show(); $('#displayAddressColumn ul').append("<li><span>" + currentRecord.addressLine2 + "</span></li>"); }
+    if (currentRecord.city != null && currentRecord.city != "") { $("#displayAddressColumn").show(); $('#displayAddressColumn ul').append("<li><span id='displayCity'>" + currentRecord.city+ "</span></li>"); }
+    if (currentRecord.provinceState != null && currentRecord.provinceState != "") {
+        $("#displayAddressColumn").show();
+        if (currentRecord.city != null && currentRecord.city != "") {
+            $('#displayCity').text(currentRecord.city + ", " + currentRecord.provinceState);
+        }
+        else {
+            $('#displayAddressColumn ul').append("<li><span>" + currentRecord.provinceState + "</span></li>");
+        }
+    }
+    if (currentRecord.postalZip != null && currentRecord.postalZip != "") { $("#displayAddressColumn").show(); $('#displayAddressColumn ul').append("<li><span>" + currentRecord.postalZip + "</span></li>"); }
+    if (currentRecord.country != null && currentRecord.country != "") { $("#displayAddressColumn").show(); $('#displayAddressColumn ul').append("<li><span>" + currentRecord.country + "</span></li>"); }
+
+    if (currentRecord.phoneNumber != null && currentRecord.phoneNumber != "") { $('#displayContactColumn, #displayPhoneItem').show(); $('#displayPhone').text(currentRecord.phoneNumber); }
+    if (currentRecord.emailAddress != null && currentRecord.emailAddress != "") { $('#displayContactColumn, #displayEmailItem').show(); $('#displayEmail').text(currentRecord.emailAddress);}
 
     // Display the panel
-    // << YOUR CODE HERE >>
+    $('#editableDisplay').hide();
+    $('#readOnlyDisplay').show();
+
 }
 
 // Edits a record when pressing the 'Edit' button or triggered after creating a new entry
 function editRecord(recordId) {
     // Set the currentRecord from the addressBookRecords
-    // << YOUR CODE HERE >>
+    addressBookRecords.forEach((element) => {
+        if (element.id == recordId) {
+            currentRecord = element;
+            $(".address-book-record[data-id='" + currentRecord.id + "']").addClass("active");
+        }
+    });
+
+    //Set current user pic inside edit window
+    var userPhoto = $(".address-book-record[data-id='" + currentRecord.id + "'] img").prop('src');
+    $('#editPhoto').attr("src", userPhoto);
+
+    //Clear photo input field
+    $("#photoUpload").val(null);
 
     // Populate the editable fields
-    // << YOUR CODE HERE >>
-
+    $('#inputFirstName').val(currentRecord.firstName);
+    $('#inputLastName').val(currentRecord.lastName);
+    $('#inputAddress').val(currentRecord.address);
+    $('#inputAddressLine2').val(currentRecord.addressLine2);
+    $('#inputCity').val(currentRecord.city);
+    $('#inputProvinceState').val(currentRecord.provinceState);
+    $('#inputPostalZip').val(currentRecord.postalZip);
+    $('#selectCountry').val(currentRecord.country);
+    $('#inputPhoneNumber').val(currentRecord.phoneNumber);
+    $('#inputEmailAddress').val(currentRecord.emailAddress);
+;
     // Show the panel and disable the Edit button
-    // << YOUR CODE HERE >>
+    $('#readOnlyDisplay').hide();
+    $('#editableDisplay').show();
+    $("#editButton").prop('disabled', true);
 }
 
 // Saves the current editing entry
 function saveEntry() {
     // Validate all entries which contain the "data-required" attribute
     // If validation fails, display an error message and an indicator on the fields
-    // << YOUR CODE HERE >>
+    var validated = true;
+
+    $("[data-required]").each(function () {
+        if (!$(this).val()) {
+            validated = false
+            $(this).css("border-color", "red");           
+        }
+    });
+
+    if (!validated) {
+        toastr.error("First and Last Name are required");
+        setTimeout(function () {
+            $("[data-required]").removeAttr("style");
+        }, 2000);
+        return;
+    }
 
     // Update the currentRecord with the data entered in
-    // << YOUR CODE HERE >>
+    currentRecord.firstName = $('#inputFirstName').val();
+    currentRecord.lastName = $('#inputLastName').val();
+    currentRecord.address = $('#inputAddress').val();
+    currentRecord.addressLine2 = $('#inputAddressLine2').val();
+    currentRecord.city = $('#inputCity').val();
+    currentRecord.provinceState = $('#inputProvinceState').val();
+    currentRecord.postalZip = $('#inputPostalZip').val();
+    currentRecord.country = $('#selectCountry').val();
+    currentRecord.phoneNumber = $('#inputPhoneNumber').val();
+    currentRecord.emailAddress = $('#inputEmailAddress').val();
 
     // Create a $formData object, attach the photo if necessary, and include the currentRecord as part of the payload
-    // << YOUR CODE HERE >>
+    var $formData = new FormData();
+
+    //Attach currentRecord keys and values into the payload
+    for (const key in currentRecord) {
+        $formData.append(key, currentRecord[key]);
+    }
+
+    //Attach photo to payload if given one
+    if ($('#photoUpload').prop('files').length > 0) {
+        $formData.append("userPhoto", $('#photoUpload')[0].files[0]);
+    }
 
     // Save the record, re-sort and display if successful
-    // << YOUR CODE HERE >>
+    $.ajax({
+        url: "Home/SaveEntry",
+        type: "POST",
+        data: $formData,
+        processData: false,
+        contentType: false,
+        beforeSend: showLoading()
+    }).done(function (data) {
+        if (data.result) {
+            addressBookRecords.forEach((element) => {
+                if (element.id == data.recordId) element = currentRecord;
+            });
+            $('#editableDisplay').hide();
+            sortRecords();
+            displayRecords(data.recordId);
+            hideLoading();
+            return;
+        }
+        hideLoading();
+        toastr.error(data.error, "Failed to Save Entry");
+    }).fail(function (xhr) {
+        hideLoading();
+        toastr.error(xhrErrorMessage(xhr), "Failed to Save Entry");
+    });
+
 }
 
 // Deletes the current editing entry
 function deleteEntry() {
     // Delete via the DeleteEntry action in the controller.
     // On successful deletion, remove the record from the addressBookRecords array and update the display
-    // << YOUR CODE HERE >>
+    $.ajax({
+        url: "Home/DeleteEntry",
+        type: "POST",
+        data: { recordid: currentRecord.id },
+        beforeSend: showLoading()
+    }).done(function (data) {
+        if (data.result) {
+            //Remove the record from the addressBookRecords
+            addressBookRecords.forEach(function (element, index, array) {
+                if (element.id == data.recordId) {
+                    array.splice(index, 1);
+                }
+            });
+
+            //Update the display
+            displayRecords();
+
+            $('#editableDisplay,#readOnlyDisplay').hide();
+
+            hideLoading();
+            return;
+        }
+        hideLoading();
+        toastr.error(data.error, "Failed to Delete Entry");
+    }).fail(function (xhr) {
+        hideLoading();
+        toastr.error(xhrErrorMessage(xhr), "Failed to Delete Entry");
+    });
 }
